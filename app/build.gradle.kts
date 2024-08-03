a8f48cc0ca4011006fedca00afb02732adc6cd3b
NinoLoncar <nino.loncar16@gmail.com>
2024-08-04 00:22:44 +0200

    Implement DataStoreManager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
---
+++ app/src/main/java/foi/nloncar/IoTAndroidApp/managers/DataStoreManager.kt
@@ -0,0 +1,29 @@
+package foi.nloncar.IoTAndroidApp.managers
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "preferences")
+
+class DataStoreManager(context: Context) {
+
+    private val dataStore = context.dataStore
+    private val preferencesKey = stringPreferencesKey("API_KEY")
+    suspend fun updateApiKey(apiKey: String) {
+        dataStore.edit { preferences ->
+            preferences[preferencesKey] = apiKey
+        }
+    }
+
+    fun getApiKey(): Flow<String?> {
+        return dataStore.data.map { preferences ->
+            preferences[preferencesKey]
+        }
+    }
+}
