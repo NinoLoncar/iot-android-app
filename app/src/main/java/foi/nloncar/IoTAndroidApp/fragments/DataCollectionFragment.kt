fb046ab1037f4e5163d25be13063657ac1cc2d11
NinoLoncar <nino.loncar16@gmail.com>
2024-08-13 14:58:05 +0200

    Add new display when collecting data

--- app/src/main/java/foi/nloncar/IoTAndroidApp/fragments/DataCollectionFragment.kt
+++ app/src/main/java/foi/nloncar/IoTAndroidApp/fragments/DataCollectionFragment.kt
@@ -12,10 +12,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -31,6 +33,8 @@ import foi.nloncar.IoTAndroidApp.managers.DataStoreManager
 import foi.nloncar.IoTAndroidApp.ws.RetrofitClient
 import foi.nloncar.IoTAndroidApp.ws.SensorData
 import foi.nloncar.IoTAndroidApp.ws.ServiceResponse
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -44,14 +48,26 @@ import java.util.Locale
 
 class DataCollectionFragment : Fragment() {
 
+    private lateinit var loadingCircle: ProgressBar
+    private lateinit var tvNumberOfDataTransfersDesc: TextView
+    private lateinit var tvNumberOfDataTransfers: TextView
+    private lateinit var tvAndroidId: TextView
+    private lateinit var tvAndroidIdDesc: TextView
+    private lateinit var btnStopDataCollection: Button
+    private lateinit var btnStoreAuthenticationKey: Button
+    private lateinit var btnStartDataCollecting: Button
+
     private lateinit var dataStoreManager: DataStoreManager
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
+    private var dataCollectionJob: Job? = null
 
     private var longitude: Double? = null
     private var latitude: Double? = null
+    private var dataCollectionInProgress: Boolean = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -80,28 +96,53 @@ class DataCollectionFragment : Fragment() {
     @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        loadingCircle = view.findViewById(R.id.pb_data_collection_in_progress)
+        tvNumberOfDataTransfersDesc = view.findViewById(R.id.tv_number_of_data_transfers_desc)
+        tvNumberOfDataTransfers = view.findViewById(R.id.tv_number_of_data_transfers)
+        tvAndroidId = view.findViewById(R.id.tv_android_id)
+        tvAndroidIdDesc = view.findViewById(R.id.tv_android_id_desc)
+        btnStopDataCollection = view.findViewById(R.id.btn_stop_data_collection)
+        btnStoreAuthenticationKey = view.findViewById(R.id.btn_store_authentication_key)
+        btnStartDataCollecting = view.findViewById(R.id.btn_start_data_collection)
+
         dataStoreManager = DataStoreManager(requireContext())
 
-        val androidIdTextView: TextView = view.findViewById(R.id.tv_android_id)
-        androidIdTextView.text = DeviceInfoHelper.getAndroidId(requireContext())
+        tvAndroidId.text = DeviceInfoHelper.getAndroidId(requireContext())
 
-        val btnStoreAuthenticationKey: Button = view.findViewById(R.id.btn_store_authentication_key)
         btnStoreAuthenticationKey.setOnClickListener {
             showAuthenticationKeySavingDialog()
         }
-        val btnStartDataCollecting: Button = view.findViewById(R.id.btn_start_data_collecting)
+
         btnStartDataCollecting.setOnClickListener {
             if (checkPrerequisitesForDataCollection()) {
+                dataCollectionInProgress = true
+                changeDisplay()
                 fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
-                lifecycleScope.launch {
-                    while (true) {
-                        delay(10000)
+                dataCollectionJob = lifecycleScope.launch {
+                    var numberOfDataTransfers = 0
+                    tvNumberOfDataTransfers.text = "0"
+                    delay(2000)
+                    while (dataCollectionInProgress) {
                         val sensorData = collectData()
-                        postData(sensorData)
+                        val success = postData(sensorData)
+                        if (success) {
+                            numberOfDataTransfers++
+                            tvNumberOfDataTransfers.text = numberOfDataTransfers.toString()
+                            delay(10000)
+                        } else
+                            dataCollectionInProgress = false
                     }
+                    changeDisplay()
                 }
             }
         }
+
+        btnStopDataCollection.setOnClickListener {
+            dataCollectionJob?.cancel()
+            dataCollectionInProgress = false
+            changeDisplay()
+        }
+
     }
 
     private fun checkPrerequisitesForDataCollection(): Boolean {
@@ -180,9 +221,10 @@ class DataCollectionFragment : Fragment() {
         return SensorData(androidId, longitude, latitude, time)
     }
 
-    private suspend fun postData(sensorData: SensorData) {
+    private suspend fun postData(sensorData: SensorData): Boolean {
         val ws = RetrofitClient.sensorDataService
         val authenticationKey = dataStoreManager.getAuthenticationKey().first()
+        val deferred = CompletableDeferred<Boolean>()
 
         ws.postSensorData(authenticationKey ?: "", sensorData).enqueue(
             object : Callback<ServiceResponse> {
@@ -190,26 +232,33 @@ class DataCollectionFragment : Fragment() {
                     call: Call<ServiceResponse>,
                     response: Response<ServiceResponse>
                 ) {
-                    val message: String = when (response.code()) {
+                    val success: Boolean
+                    when (response.code()) {
                         200 -> {
-                            "Podaci dodani"
+                            success = true
                         }
 
                         403 -> {
-                            "Uređaj nije registirian ili je autentifikacijski ključ pogrešan"
+                            success = false
+                            showShortToast("Uređaj nije registirian ili je autentifikacijski ključ pogrešan")
+
                         }
 
                         else -> {
-                            "Dogodila se greška"
+                            success = false
+                            showShortToast("Dogodila se greška")
                         }
                     }
-                    showShortToast(message)
+
+                    deferred.complete(success)
                 }
 
                 override fun onFailure(call: Call<ServiceResponse>, t: Throwable) {
                     showShortToast("Dogodila se greška")
+                    deferred.complete(false)
                 }
             })
+        return deferred.await()
     }
 
     private fun openAppSettings() {
@@ -230,6 +279,18 @@ class DataCollectionFragment : Fragment() {
             " HH:mm:ss dd.MM.yyyy",
             Locale.getDefault()
         ).format(Date(currentTime))
+    }
+
+    private fun changeDisplay() {
+        loadingCircle.isVisible = dataCollectionInProgress
+        tvNumberOfDataTransfersDesc.isVisible = dataCollectionInProgress
+        tvNumberOfDataTransfers.isVisible = dataCollectionInProgress
+        btnStopDataCollection.isVisible = dataCollectionInProgress
+
+        tvAndroidId.isVisible = !dataCollectionInProgress
+        tvAndroidIdDesc.isVisible = !dataCollectionInProgress
+        btnStoreAuthenticationKey.isVisible = !dataCollectionInProgress
+        btnStartDataCollecting.isVisible = !dataCollectionInProgress
     }
 
 }
